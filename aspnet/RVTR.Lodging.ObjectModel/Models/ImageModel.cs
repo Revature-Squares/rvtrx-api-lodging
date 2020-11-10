909716ff26fba2f34a17ab262a40b4704acd5303
Alex Walker <atwalk14@gmail.com>
2020-11-10 12:09:19 -0500

    adds data annotation for image uri

--- aspnet/RVTR.Lodging.ObjectModel/Models/ImageModel.cs
+++ aspnet/RVTR.Lodging.ObjectModel/Models/ImageModel.cs
@@ -5,10 +5,20 @@ namespace RVTR.Lodging.ObjectModel.Models
 {
   public class ImageModel : IValidatableObject
   {
+    /// <summary>
+    /// Id number for image
+    /// </summary>
     public int Id { get; set; }
 
+    /// <summary>
+    /// Lodging model id (foreign key)
+    /// </summary>
     public int? LodgingModelId { get; set; }
 
+    /// <summary>
+    /// Image uri for the image
+    /// </summary>
+    [RegularExpression(@"^(http(s?):\/\/)[^\s]*$", ErrorMessage = "Image URI must be a real image URI.")]
     public string ImageUri { get; set; } = "https://bulma.io/images/placeholders/1280x960.png";
 
     /// <summary>
